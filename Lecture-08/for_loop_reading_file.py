e031b7260d383e8327a56e72405dc0ce633a6cbf
matee-kmutnb <s6906022511055@email.kmutnb.ac.th>
2026-09-01 17:33:25 +0700

    commit 2

---
+++ Lecture-08/for_loop_reading_file.py
@@ -0,0 +1,10 @@
+# This program uses the for loop to read
+ # all of the values in the sales.txt file.
+ # Open the sales.txt file for reading using with statement.
+with open('sales.txt', 'r') as sales_file:
+ # Read all the lines from the file.
+ for line in sales_file:
+ # Convert line to a float.
+    amount = float(line)
+ # Format and display the amount.
+ print(format(amount, '.2f'))
