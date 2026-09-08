ff97e59a85b628d6bde1aa824e0f74a23825023e
matee-kmutnb <s6906022511055@email.kmutnb.ac.th>
2026-09-08 21:55:25 +0700

    Week12-Commit1

---
+++ Lecture-09/Example_of_Common_Exceptions.py
@@ -0,0 +1,8 @@
+# Example of common exceptions
+try:
+    x = 1 / 0 # ZeroDivisionError
+except ZeroDivisionError as e:
+    print(f"Error: {e}")
+
+print("End of program")
+#Output : Error: division by zero
